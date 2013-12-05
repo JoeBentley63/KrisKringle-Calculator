d21adee8b3b46cf38dac159670e251257f036744
Joseph Bentley <seppybentley@gmail.com>
2013-12-05 20:20:59 +0000

    Kris Kringle
    A VS 2012 C# forms project that calculates Kris Kringle Pairs based on
    names entered

---
+++ KrisKringle/KrisKringle/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace KrisKringle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.scrollTextBox2 = new ScrollTextBox();
+            this.scrollTextBox1 = new ScrollTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(255, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(274, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enter ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(204, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Calculate Pairs";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Names : 0 ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Results";
+            // 
+            // scrollTextBox2
+            // 
+            this.scrollTextBox2.Cursor = System.Windows.Forms.Cursors.No;
+            this.scrollTextBox2.Location = new System.Drawing.Point(13, 55);
+            this.scrollTextBox2.Multiline = true;
+            this.scrollTextBox2.Name = "scrollTextBox2";
+            this.scrollTextBox2.ReadOnly = true;
+            this.scrollTextBox2.Size = new System.Drawing.Size(185, 92);
+            this.scrollTextBox2.TabIndex = 3;
+            // 
+            // scrollTextBox1
+            // 
+            this.scrollTextBox1.Location = new System.Drawing.Point(13, 172);
+            this.scrollTextBox1.Multiline = true;
+            this.scrollTextBox1.Name = "scrollTextBox1";
+            this.scrollTextBox1.ReadOnly = true;
+            this.scrollTextBox1.Size = new System.Drawing.Size(320, 78);
+            this.scrollTextBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(345, 265);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.scrollTextBox2);
+            this.Controls.Add(this.scrollTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(361, 303);
+            this.MinimumSize = new System.Drawing.Size(361, 303);
+            this.Name = "Form1";
+            this.Text = "Joe Bentley - Kris Kringle Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private ScrollTextBox scrollTextBox1;
+        private ScrollTextBox scrollTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ KrisKringle/KrisKringle/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KrisKringle
+{
+
+    public struct Pair
+    {
+        public string _personA;
+        public string _personB;
+    }
+
+    public partial class Form1 : Form
+    {
+        List<string> _names;
+        List<string> _ProcessedNames;
+        List<Pair> _pairs;
+        Random random;
+        public Form1()
+        {
+            _names = new List<string>();
+            random = new Random();
+            
+
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                string _tempName = textBox1.Text;
+                _names.Add(_tempName);
+                label1.Text = "Names : " + _names.Count;
+                textBox1.Clear();
+                string _nameString = "";
+                for (int i = 0; i < _names.Count; i++)
+                {
+                    _nameString += _names[i] + "\r\n";
+                }
+                scrollTextBox2.Text = _nameString;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //run algorithm
+            if (_names.Count % 2 != 0)
+            {
+                scrollTextBox1.Text = "Un-even number of participants";
+            }
+            else
+            {
+                _pairs = new List<Pair>();
+                _ProcessedNames = new List<string>();
+
+                string _next = FindFreePerson();
+                int _num = 0;
+                bool _found = false;
+                for (int i = 0; i < _names.Count; i++)
+                {
+                    if (_names[i] == _next)
+                    {
+                        _found = true;
+                        _num = i;
+                    }
+                }
+                if (_found == false)
+                {
+                    Console.WriteLine("Shit");
+                }
+                _names.Remove(_next);
+                ProcessPerson(_num, _next);
+            }
+        }
+
+        private void ProcessPerson(int _person,string _name)
+        {
+            for (bool _continue = false; _continue == false; )
+            {
+                string _pair = FindFreePerson();
+                _names.Remove(_pair);
+                if (_name == _pair)
+                {
+                    _continue = false;
+                }
+                else
+                {
+                    _continue = true;
+                    Pair _newPair;
+                    _newPair._personA = _name;
+                    _newPair._personB = _pair;
+                    _pairs.Add(_newPair);
+                    _names.Remove(_pair);
+                    if (_names.Count == 0)
+                    {
+                        PrintPairs();
+                    }
+                    else
+                    {
+                        string _next = FindFreePerson();
+                        int _num = 0;
+                        bool _found = false;
+                        for(int i = 0; i < _names.Count; i ++)
+                        {
+                            if(_names[i] == _next)
+                            {
+                                _found = true;
+                                _names.Remove(_next);
+                                _num = i;
+                            }
+                        }
+                        if (_found == false)
+                        {
+                            Console.WriteLine("Shit");
+                        }
+
+                        ProcessPerson(_num, _next);
+                    }
+                }
+            }
+            
+           
+        }
+        private void PrintPairs()
+        {
+            string _pairString = "";
+            for (int i = 0; i < _pairs.Count; i++)
+            {
+                _pairString += _pairs[i]._personA + " is matched with : " + _pairs[i]._personB + "\r\n";
+            }
+            scrollTextBox1.Clear();
+          //  scrollTextBox2.Clear();
+            scrollTextBox1.Text = "Results : \r\n" + _pairString;
+            //MessageBox.Show(_PairString);
+         }
+        private string FindFreePerson()
+        {
+            if (_names.Count == 0)
+            {
+                return null;
+            }
+            string _person = "";
+            int _randomNumber = 1;
+            try
+            {
+                _randomNumber = random.Next(1, _names.Count - 1);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                _randomNumber = 0;
+            }
+            _person = _names[_randomNumber];
+            return _person;
+        }
+    }
+}
